7e6e936d0087c89f97a3ee8c769a5b588207d20e
Muhamad Sholeh <87067730+msholehdev@users.noreply.github.com>
2023-12-21 15:04:33 +0700

    Update main.c

--- math/main.c
+++ math/main.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
 
+bool is_even(const int x){
+    return (x%2==0)?true:false;
+}
+float celtofah(const int c){
+    return ((9*c)/5)+32;
+}
+int main() {
+  int x = 5;
+  if(is_even(x)){
+    printf("The number is Even. \n");
+  }else{
+    printf("The number is Odd. \n");
+  }
+
+  printf("Fahrenheit dari 36 adalah %f",celtofah(36));
+  
+  return 0;
+}
